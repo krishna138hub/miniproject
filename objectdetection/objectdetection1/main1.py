799d8d91f34b7078f47499910661e5d725657208
Krishnapriya <krishnapriyata3@gmail.com>
2026-03-06 01:30:52 +0530

    the one files with coco models

---
+++ objectdetection/objectdetection1/main1.py
@@ -0,0 +1,80 @@
+import cv2
+
+from waste1 import detectwaste
+from wastetracking1 import WasteTracker
+from hands1 import detecthands
+from face1 import detectface
+from roi1 import detectroi, is_inside_roi
+
+
+def main():
+
+    print("Littering Detection System")
+    print("Press ESC to exit\n")
+
+    video_path = "photos2/bad.mp4"
+    cap = cv2.VideoCapture(video_path)
+
+    waste_tracker = WasteTracker(
+        separation_threshold=80,
+        littering_time_threshold=5.0
+    )
+    second=0
+    while True:
+
+        cap.set(cv2.CAP_PROP_POS_MSEC, second * 200)
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+
+        # BIN ROI
+        roi_results = detectroi(frame)
+
+        # Face detection
+        detectface(frame)
+
+        # Hand detection
+        hand_results = detecthands(frame)
+
+        # Waste detection
+        waste_boxes = detectwaste(frame)
+
+        # Ignore waste inside bin
+        filtered_waste = []
+        for waste in waste_boxes:
+            if not is_inside_roi(waste["bbox"], roi_results.box):
+                filtered_waste.append(waste)
+
+        littered_now = waste_tracker.update(
+            hand_results,
+            filtered_waste,
+            frame
+        )
+
+        frame = waste_tracker.draw(frame)
+
+        # Resize screen for visualization
+        screen_w = 1280
+        screen_h = 720
+
+        h, w = frame.shape[:2]
+
+        if w > screen_w or h > screen_h:
+            scale = min(screen_w / w, screen_h / h)
+            frame = cv2.resize(frame, (int(w * scale), int(h * scale)))
+
+        cv2.imshow("Littering Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+        second=second+1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ objectdetection/objectdetection1/waste1.py
@@ -0,0 +1,44 @@
+from ultralytics import YOLO
+import gc
+
+model = YOLO("yolov8s.pt")
+
+target_objects = ["bottle", "remote","frisbee","tennis racket"]
+
+conf_threshold = 0.1
+img_size = 960
+
+track_id = 0
+
+
+def detectwaste(frame):
+
+    global track_id
+
+    results = model(frame, conf=conf_threshold, imgsz=img_size)[0]
+
+    waste_boxes = []
+
+    for box in results.boxes:
+
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        cls = int(box.cls[0])
+        conf = float(box.conf[0])
+
+        class_name = model.names[cls]
+
+        if class_name in target_objects:
+
+            waste_boxes.append({
+                "id": track_id,
+                "bbox": (x1, y1, x2, y2),
+                "label": class_name,
+                "conf": conf
+            })
+            print("Detected:", class_name, "Confidence:", conf)
+            print('TRACK ID:', track_id)
+            track_id = 0  # Only one object tracked
+    print("waste_boxes:", waste_boxes)
+    del results
+    gc.collect()
+    return waste_boxes
